001513688499e5299f090d5a8d782d83155616b5
Evgeny Kuptsov <kupzov200@mail.ru>
2024-04-09 13:05:22 +0400

    feat: add solution for task "partialUsingArguments"

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -199,8 +199,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function partialUsing(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
