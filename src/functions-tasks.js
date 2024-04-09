d7d4c786b04715fa7eb4514fb4d6a3f3ce2adacb
Evgeny Kuptsov <kupzov200@mail.ru>
2024-04-09 13:01:00 +0400

    feat: add solution for task "getIdGeneratorFunction"

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -220,8 +220,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return function get() {
+    const startCurr = start;
+    start += 1;
+    return startCurr;
+  };
 }
 
 module.exports = {
