1b2f5cbeb7d3bbcbef831db376aa56658468b6fb
Evgeny Kuptsov <kupzov200@mail.ru>
2026-01-13 00:06:47 +0400

    feat: add resolve for logger task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -184,8 +184,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function log(...args) {
+    const argString = args.map((arg) => JSON.stringify(arg)).join(',');
+    logFunc(`${func.name}(${argString}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argString}) ends`);
+    return result;
+  };
 }
 
 /**
