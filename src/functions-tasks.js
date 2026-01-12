e14ef2e91fceb20d87b901e95a8daf13963f7538
Evgeny Kuptsov <kupzov200@mail.ru>
2026-01-12 23:37:39 +0400

    feat: add resolve for retry task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -146,8 +146,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function use() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        if (i === attempts - 1) {
+          throw new Error();
+        }
+      }
+    }
+    return null;
+  };
 }
 
 /**
